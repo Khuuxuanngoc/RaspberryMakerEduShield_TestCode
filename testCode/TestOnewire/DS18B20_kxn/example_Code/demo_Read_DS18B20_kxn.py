e2d30c151d027532ecc1002b189c221422096957
xuanngoc1992 <xuanngoc1992@gmail.com>
2022-06-14 10:40:08 +0700

    add info to demo..DS18B20_kxn.py

--- testCode/TestOnewire/DS18B20_kxn/example_Code/demo_Read_DS18B20_kxn.py
+++ testCode/TestOnewire/DS18B20_kxn/example_Code/demo_Read_DS18B20_kxn.py
@@ -1,8 +1,12 @@
 """
     Step 1: Edit config.txt
+                cd /boot
+                sudo nano config.txt
+
             Add two line:
                 dtoverlay=w1-gpio
                 dtoverlay=w1-gpio,gpiopin=19
+                (https://raspberryautomation.com/connect-multiple-ds18b20-temperature-sensors-to-a-raspberry-pi/)
     Step 2: Install library
             sudo pip3 install w1thermsensor
             (https://bigl.es/ds18b20-temperature-sensor-with-python-raspberry-pi/)
